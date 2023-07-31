c3155e3fb2db84709c5d05e0b2dd46d86ef88676
Ravi <ravik@technoarchsoftwares.com>
2023-07-31 10:55:53 +0530

    notes like and user followers and following features apis added

--- note_taker_project/notes/serializers.py
+++ note_taker_project/notes/serializers.py
@@ -3,14 +3,14 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
-from notes.models import Note, NoteSharingInvitation
+from notes.models import Note, NoteSharingInvitation, UserRelationship
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('email', 'password')
+        fields = ('username', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
